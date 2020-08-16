5bcaf7231354cf3185ab93ab89c0b612baadeb96
Pranjal Kole <61913668+ghastly-qurk@users.noreply.github.com>
2020-08-16 12:25:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+async function fetchWeather(zip, api_key) {
+  const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},in&appid=${api_key}`);
+  const weather = await resp.json();
+  const div = document.querySelector('div');
+  week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  month = ["January", "February", "March", "April", "May", "June", "July", "August", "September" , "October", "November", "December"];
+  date = new Date();
+  if (date.getHours() > 12) {
+    div.innerHTML = `<h3 id='lesspad'>${weather.name}, ${weather.sys.country}</h3><p class='date' id='lesspad'>${date.getHours() - 12}:${date.getMinutes()} PM, ${week[date.getDay()]}, ${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}</p><h2>${+((weather.main.temp - 273.15).toFixed(1))}°C</h2><span><p id="bold">Feels like ${+((weather.main.feels_like - 273.15).toFixed(1))}°C. ${weather.weather[0].main}. ${weather.weather[0].description[0].toUpperCase() + weather.weather[0].description.substring(1)}</p><p>Humidity: ${weather.main.humidity}%</p></span>`;
+  } else {
+    div.innerHTML = `<h3 id='lesspad'>${weather.name}, ${weather.sys.country}</h3><p class='date' id='lesspad'>${date.getHours()}:${date.getMinutes()} AM, ${week[date.getDay()]}, ${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}</p><h2>${+((weather.main.temp - 273.15).toFixed(1))}°C</h2><span><p id="bold">Feels like ${+((weather.main.feels_like - 273.15).toFixed(1))}°C. ${weather.weather[0].main}. ${weather.weather[0].description[0].toUpperCase() + weather.weather[0].description.substring(1)}</p><p>Humidity: ${weather.main.humidity}%</p></span>`;
+  }
+}
+
+async function main() {
+   fetchWeather('700055', '4b17c454b99629cb223d1ae103aa7696');
+}
+
+main();
