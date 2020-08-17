901596ef92953e543824b7f6a6ac0a33ff83a293
Pranjal Kole <61913668+ghastly-qurk@users.noreply.github.com>
2020-08-17 13:57:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function fetchWeather() {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=700055,in&appid=4b17c454b99629cb223d1ae103aa7696`)
-  .then(resp => resp.json())
-  .then(
-      weather => {
-          const div = document.querySelector('div');
-          week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-          month = ["January", "February", "March", "April", "May", "June", "July", "August", "September" , "October", "November", "December"];
+async function fetchWeather() {
+  const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=700055,in&appid=4b17c454b99629cb223d1ae103aa7696`);
+  const weather = await resp.json();
+  const div = document.querySelector('div');
+  week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  month = ["January", "February", "March", "April", "May", "June", "July", "August", "September" , "October", "November", "December"];
           date = new Date();
           if (date.getHours() > 12) {
             if (date.getMinutes() < 10) {
@@ -20,8 +18,6 @@ function fetchWeather() {
               div.innerHTML = `<h3 id='lesspad'>${weather.name}, ${weather.sys.country}</h3><p class='date' id='lesspad'>${date.getHours()}:${date.getMinutes()} AM, ${week[date.getDay()]}, ${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}</p><h2>${+((weather.main.temp - 273.15).toFixed(1))}°C</h2><span><p id="bold">Feels like ${+((weather.main.feels_like - 273.15).toFixed(1))}°C. ${weather.weather[0].main}. ${weather.weather[0].description[0].toUpperCase() + weather.weather[0].description.substring(1)}</p><p>Humidity: ${weather.main.humidity}%</p></span>`;
 }
 }
-        }
-  );
 }
 
 function main() {
