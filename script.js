a2d4cc85772962c811054cf1040db051f9a4a112
Pranjal Kole <61913668+ghastly-qurk@users.noreply.github.com>
2020-08-25 10:16:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 async function getWeather() {
-	return await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=700055,in&appid=4b17c454b99629cb223d1ae103aa7696`);
+	return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=700055,in&appid=4b17c454b99629cb223d1ae103aa7696`);
 }
 
 async function weatherJSON() {
