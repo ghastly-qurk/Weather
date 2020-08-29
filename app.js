ca211fb1a1b61eb90c2be38d10ffd8c075534548
Pranjal Kole <pranjal.kole7@gmail.com>
2020-08-29 20:47:21 +0530

    Update style.css

--- app.js
+++ app.js
@@ -47,11 +47,12 @@ async function main() {
   }:${date.getMinutes()} PM, ${week[date.getDay()]}, ${
     month[date.getMonth()]
   } ${date.getDate()}, ${date.getFullYear()}</p>
-<h1 class='my-3 py-3 light'><img src='http://openweathermap.org/img/wn/${
+<div>
+<img src='http://openweathermap.org/img/wn/${
     weather.weather[0].icon
-  }@2x.png' height='50' width='50' />${+(weather.main.temp - 273.15).toFixed(
-    1
-  )}°C</h1>
+  }@2x.png' height='50' width='50' />
+<h1 class='my-3 py-3 light'>${+(weather.main.temp - 273.15).toFixed(1)}°C</h1>
+</div>
 <div class='bold'>
 <p class='my-0 py-0'>Feels like ${+(weather.main.feels_like - 273.15).toFixed(
     1
