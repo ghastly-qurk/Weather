49ca1b7108b5099fb50889a68b68eac04049c109
Pranjal Kole <pranjal.kole7@gmail.com>
2020-08-31 20:49:13 +0530

    sksksksk

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 async function fetchWeather() {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?zip=700055,in&appid=4b17c454b99629cb223d1ae103aa7696`
+    "https://api.openweathermap.org/data/2.5/weather?zip=700055,in&appid=4b17c454b99629cb223d1ae103aa7696"
   );
 }
 
@@ -13,73 +13,83 @@ async function weatherJSON() {
 async function main() {
   const weather = await weatherJSON();
 
-  const div = document.querySelector(`div#weather`);
+  const div = document.querySelector("div#weather");
 
   const date = new Date();
   const week = [
-    `Sunday`,
-    `Monday`,
-    `Tuesday`,
-    `Wednesday`,
-    `Thursday`,
-    `Friday`,
-    `Saturday`,
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
   ];
   const month = [
-    `January`,
-    `February`,
-    `March`,
-    `April`,
-    `May`,
-    `June`,
-    `July`,
-    `August`,
-    `September`,
-    `October`,
-    `November`,
-    `December`,
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
   div.innerHTML = `
-<h3 class='mb-0 pb-0'>${weather.name}, ${weather.sys.country}</h3>
-<p class='my-0 py-0 text-secondary'>${
-    date.getHours() - 12
-  }:${date.getMinutes()} PM, ${week[date.getDay()]}, ${
+  <h3 class='mb-0 pb-0'>
+    ${weather.name}, ${weather.sys.country}
+  </h3>
+  <p class='my-0 py-0 text-secondary'>
+    ${date.getHours() - 12}:${date.getMinutes()} PM, ${week[date.getDay()]}, ${
     month[date.getMonth()]
-  } ${date.getDate()}, ${date.getFullYear()}</p>
-<h1 class='my-3 py-3 light'><img src='http://openweathermap.org/img/wn/${
-    weather.weather[0].icon
-  }@2x.png' height='50' width='50' />${+(weather.main.temp - 273.15).toFixed(
-    1
-  )}°C</h1>
-<div class='bold'>
-<p class='my-0 py-0'>Feels like ${+(weather.main.feels_like - 273.15).toFixed(
-    1
-  )}°C. ${weather.weather[0].main}. ${
+  } ${date.getDate()}, ${date.getFullYear()}
+  </p>
+
+  <h1 class='inline-flex center-y my-3 py-3'>
+    <img
+      src='http://openweathermap.org/img/wn/${
+        weather.weather[0].icon
+      }@4x.png' height='50' width='50' />
+    ${+(weather.main.temp - 273.15).toFixed(1)}°C
+  </h1>
+
+  <div class='bold'>
+    <p class='my-0 py-0'>
+      Feels like ${+(weather.main.feels_like - 273.15).toFixed(1)}°C. ${
+    weather.weather[0].main
+  }. ${
     weather.weather[0].description[0].toUpperCase() +
     weather.weather[0].description.substring(1)
-  }</p>
-<p class='my-0 py-0'>Humidity: ${weather.main.humidity}%</p>
-</div>
+  }
+    </p>
+    <p class='my-0 py-0'>
+      Humidity: ${weather.main.humidity}%
+    </p>
+  </div>
 `;
 
   if (date.getHours() <= 12) {
     document.querySelector(
-      `p.text-secondary`
+      "p.text-secondary"
     ).textContent = `${date.getHours()}:${date.getMinutes()} AM, ${
       week[date.getDay()]
     }, ${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 
     if (date.getMinutes() < 10) {
       document.querySelector(
-        `p.text-secondary`
+        "p.text-secondary"
       ).textContent = `${date.getHours()}:0${date.getMinutes()} AM, ${
         week[date.getDay()]
       }, ${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
     }
   } else {
     if (date.getMinutes() < 10) {
-      document.querySelector(`p.text-secondary`).textContent = `${
+      document.querySelector("p.text-secondary").textContent = `${
         date.getHours() - 12
       }:0${date.getMinutes()} PM, ${week[date.getDay()]}, ${
         month[date.getMonth()]
